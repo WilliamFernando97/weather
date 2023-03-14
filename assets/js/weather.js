801a9650a814f80916c5842cc723d8214731612a
WilliamFernando97 <70359210+WilliamFernando97@users.noreply.github.com>
2023-03-13 22:53:36 -0500

    commit parte 2

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -75,12 +75,50 @@ let loadDayForecastData = () => {
 
 let loadWeekForecastData = () => {
     let data = weather_data ;
+    let week_data = '';
+    for (let item of data) {
+        for(let idx of item.forecast_week){
+            let { day, text, date, temperature, icon } = idx;
+            let { min, max } = temperature;
     
+            week_data += `
+            <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+            <div class="d-flex flex-column">
+                <h6 class="mb-1 text-dark font-weight-bold text-sm">${text}</h6>
+                <span class="text-xs">${date}</span>
+            </div>
+            <div class="d-flex align-items-center ">
+                <span class="font-weight-bold text-dark mx-2">${max}</span>|<span class="text-dark mx-2">${min}</span>
+                <div class="ms-4"><i class="material-icons fs-2 me-1 rainy">${icon}</i></div>
+            </div>
+            </li>
+            `;
     
+            let weekforecast_gye = document.getElementsByClassName("list-group");
+            weekforecast_gye[0].innerHTML = week_data    
+
+        }
+
+      }
 
 	
 }
 
 
+
+
+let drop_id = document.getElementById('loadinfo');
+
+drop_id.addEventListener('click', () => {
+    loadWeekForecastData();
+});
+
+
+
+
+
+
 loadDayForecastData();
-loadWeekForecastData();
+//loadWeekForecastData();
+
+ 
